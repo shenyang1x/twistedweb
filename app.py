627c2968255f7f81133d4cc7785cd3da1f589168
shenyang <yangx.shen@intel.com>
2018-02-26 16:53:48 +0800

    test

--- app.py
+++ app.py
@@ -53,10 +53,5 @@ if __name__ == '__main__':
     factory = Site(Root())
     reactor.listenTCP(8880, factory)
     reactor.callLater(30, Echo)
-    print "the reactor is run"
-    print "test"
-    print "test"
-    print "test"
-    print "test"
-    print "test"
+
     reactor.run()
